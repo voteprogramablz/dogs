f849dde17121316f6358523a478ea6767048b505
André R. de Melo <andrermelo1@hotmail.com>
2021-09-20 01:59:25 -0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Form/Input";
+import Button from "../Form/Button";
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json.token);
+      });
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
