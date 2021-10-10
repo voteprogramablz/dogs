0ff745a27b98e9747b34d9adc8303a362934893c
André R. de Melo <andrermelo1@hotmail.com>
2021-10-10 19:08:00 -0300

    Início do desenvolvimento da página de Usuário, criação dos botões de Navegação da página Conta e estilização dela

---
+++ src/Components/User/UserHeader.js
@@ -0,0 +1,34 @@
+import React from "react";
+import UserHeaderNav from "./UserHeaderNav";
+import styles from "./UserHeader.module.css";
+import { useLocation } from "react-router";
+
+const UserHeader = () => {
+  const [title, setTitle] = React.useState("");
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    switch (pathname) {
+      case "/conta":
+        setTitle("Minha Conta");
+        break;
+      case "/conta/estatisticas":
+        setTitle("Estatísticas");
+        break;
+      case "/conta/postar":
+        setTitle("Postar uma foto");
+        break;
+      default:
+        setTitle("Oie");
+    }
+  }, [pathname]);
+
+  return (
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
+      <UserHeaderNav />
+    </header>
+  );
+};
+
+export default UserHeader;
